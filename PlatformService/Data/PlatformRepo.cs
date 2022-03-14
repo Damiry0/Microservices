04d816d1e79418d8f33961b32652cf70b16fddc8
Damian Ryś <55887851+Damiry0@users.noreply.github.com>
2022-03-14 20:27:09 +0100

    Update PlatformRepo.cs

--- PlatformService/Data/PlatformRepo.cs
+++ PlatformService/Data/PlatformRepo.cs
@@ -1,35 +1,42 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using PlatformService.Models;
 
 namespace PlatformService.Data
 {
     public class PlatformRepo : IPlatformRepo
     {
-        private readonly AppDbContext _conext;
+        private readonly AppDbContext _context;
 
         public PlatformRepo(AppDbContext context)
         {
-            _conext = context;
+            _context = context;
         }
 
         public void CreatePlatform(Platform plat)
         {
-            throw new System.NotImplementedException();
+            if(plat==null)
+            {
+                throw new ArgumentNullException(nameof(plat));
+            }
+
+            _context.Platforms.Add(plat);
         }
 
         public IEnumerable<Platform> GetAllPlatforms()
         {
-            throw new System.NotImplementedException();
+            return _context.Platforms.ToList();
         }
 
         public Platform GetPlatformById(int id)
         {
-            throw new System.NotImplementedException();
+            return _context.Platforms.FirstOrDefault(x => x.Id==id);
         }
 
         public bool SaveChanges()
         {
-            throw new System.NotImplementedException();
-        }
+            return (_context.SaveChanges()>=0);
+        }   
     }
 }
